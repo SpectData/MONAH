98740377e02cef29c25942066caecc43af09970c
mfmakahiya <44262531+mfmakahiya@users.noreply.github.com>
2021-01-06 05:13:46 +0000

    main script changes

--- Python/Data_Preprocessing/Stage_3/main.py
+++ Python/Data_Preprocessing/Stage_3/main.py
@@ -42,10 +42,10 @@ def weave_vpa(video_1, video_2, delay, tone, speech_rate, au_action, posiface, s
     cmf.run_creating_directories(video_1, video_2, parallel_run_settings)
     # TODO: write if statements to detect if we can skip some steps
     # exa.run_extracting_audio(parallel_run_settings)
-    gst.run_google_speech_to_text(video_1, video_2, parallel_run_settings)
-    opf.run_open_face(video_1, video_2, parallel_run_settings)
-    wvt.run_weaving_talkturn(video_1, video_2, parallel_run_settings)
-    exv.run_vokaturi(video_1, video_2, parallel_run_settings)
+    # gst.run_google_speech_to_text(video_1, video_2, parallel_run_settings)
+    # opf.run_open_face(video_1, video_2, parallel_run_settings)
+    # wvt.run_weaving_talkturn(video_1, video_2, parallel_run_settings)
+    # exv.run_vokaturi(video_1, video_2, parallel_run_settings)
     print("Done data processing - Stage 1")
 
     # Stage 2 runs - processed tables
--- Python/Data_Preprocessing/Stage_3/test_main.py
+++ Python/Data_Preprocessing/Stage_3/test_main.py
@@ -1,100 +1,146 @@
 '''
-This script tests the accurateness of the main script results
+Tests ./main.py
 '''
 
-import os
+import Python.Data_Preprocessing.config.dir_config as prs
+import Python.Data_Preprocessing.Stage_3.main as main
 
-import pandas as pd
 
-
-def talkturn_lookup(talkturn_no, family):
+class Test_Narrative:
     '''
-    Look up table for expeceted text blob result
-    :return:
+    Scoring Test
     '''
-    talkturn = {'talkturn_no': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                                1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                                1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                'family': ['v', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'v',
-                           'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp',
-                           'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa'],
-                'talkturn': ['the woman said excuse me.',
-                             'the man said do you have your forms.',
-                             'the woman said yeah.',
-                             'the man said let me see them.',
-                             'the woman said is there a problem.',
-                             'the man said who told you to get in this line.',
-                             'the woman said you did.',
-                             'the woman said you were standing at the beginning and you directed me.',
-                             'the man said okay but i didnt tell you to get in this line if you are filling out this particular form.',
-                             'the woman said well whats the problem  let me change it.',
-                             'the woman said excuse me.',
-                             'the man said do you have your forms.',
-                             'the woman said yeah.',
-                             'the man said let me see them.',
-                             'after two hundred milliseconds a slightly long delay the woman said is there a problem.',
-                             'the man happily and very quickly said who told you to get in this line.',
-                             'the woman sadly and quickly said you did.',
-                             'the woman said you were standing at the beginning and you directed me.',
-                             'the man angrily and very quickly said okay but i didnt tell you to get in this line if you are filling out this particular form.',
-                             'the woman said well whats the problem  let me change it.',
-                             'the woman said excuse me.',
-                             'the man said do you have your forms.',
-                             'the woman said yeah.',
-                             'the man said let me see them.',
-                             'the woman said is there a problem.',
-                             'the man said who told you to get in this line.',
-                             'the woman said you did.',
-                             'the woman said you were standing at the beginning and you directed me.',
-                             'the man said okay but i didnt tell you to get in this line if you are filling out this particular form.',
-                             'the woman said well whats the problem  let me change it.',
-                             'the woman said excuse me.',
-                             'the man said do you have your forms.',
-                             'the woman said yeah.',
-                             'the man said let me see them.',
-                             'after two hundred milliseconds a slightly long delay the woman said is there a problem.',
-                             'the man happily and very quickly said who told you to get in this line.',
-                             'the woman sadly and quickly said you did.',
-                             'the woman said you were standing at the beginning and you directed me.',
-                             'the man angrily and very quickly said okay but i didnt tell you to get in this line if you are filling out this particular form.',
-                             'the woman said well whats the problem  let me change it.'
-                             ]}
-    df = pd.DataFrame(data=talkturn)
+    computer_name = 'marriane_win'
 
-    return df.loc[(df['talkturn_no'] == talkturn_no) & (df['family'] == family), 'talkturn'].values[0]
+    parallel_run_settings = prs.get_parallel_run_settings(computer_name)
+    talkturn_lookup = {'talkturn_no': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                       1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                       1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                       'family': ['v', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'v', 'v',
+                                  'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp', 'vp',
+                                  'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa', 'vpa'],
+                       'talkturn': ['the woman said excuse me.',
+                                    'the man said do you have your forms.',
+                                    'the woman said yeah.',
+                                    'the man said let me see them.',
+                                    'the woman said is there a problem.',
+                                    'the man said who told you to get in this line.',
+                                    'the woman said you did.',
+                                    'the woman said you were standing at the beginning and you directed me.',
+                                    'the man said okay but i didnt tell you to get in this line if you are filling out this particular form.',
+                                    'the woman said well whats the problem  let me change it.',
+                                    'the woman said excuse me.',
+                                    'the man said do you have your forms.',
+                                    'the woman said yeah.',
+                                    'the man said let me see them.',
+                                    'after two hundred milliseconds a slightly long delay the woman said is there a problem.',
+                                    'the man happily and very quickly said who told you to get in this line.',
+                                    'the woman sadly and quickly said you did.',
+                                    'the woman said you were standing at the beginning and you directed me.',
+                                    'the man angrily and very quickly said okay but i didnt tell you to get in this line if you are filling out this particular form.',
+                                    'the woman said well whats the problem  let me change it.',
+                                    'the woman said excuse me.',
+                                    'the man said do you have your forms.',
+                                    'the woman said yeah.',
+                                    'the man said let me see them.',
+                                    'after two hundred milliseconds a slightly long delay the woman said is there a problem.',
+                                    'the man happily and very quickly said who told you to get in this line.',
+                                    'the woman sadly and quickly said you did.',
+                                    'the woman said you were standing at the beginning and you directed me.',
+                                    'the man angrily and very quickly said okay but i didnt tell you to get in this line if you are filling out this particular form.',
+                                    'the woman said well whats the problem  let me change it.'
+                                    ]
+                       }
 
-def test_blob_accurateness(video_name_1, video_name_2, parallel_run_settings):
-    '''
+    # Test case #1 - verbatim
+    def test_v(self):
+        '''
+        Check that verbatim results are all matching
+        :return: assertion
+        '''
 
-    :return:
-    '''
+        parallel_run_settings = Test_Narrative.parallel_run_settings
+        expected = Test_Narrative.talkturn_lookup
+        actual = main.weave_vpa(video_1='Ses01F_F',
+                                video_2='Ses01F_M',
+                                delay=0,
+                                tone=0,
+                                speech_rate=0,
+                                au_action=0,
+                                posiface=0,
+                                smile=0,
+                                parallel_run_settings=parallel_run_settings
+                                )
 
-    # parallel_run_settings = prs.get_parallel_run_settings("marriane_win")
-    actual = pd.read_csv(os.path.join(parallel_run_settings['csv_path'],
-                                      video_name_1 + '_' + video_name_2,
-                                      'Stage_3',
-                                      'narrative_fine.csv'))
-    matching_status = []
-    for family in actual.family.unique():
-        for talkturn_no in range(1,11):
-            df_expected = talkturn_lookup(talkturn_no, family)
-            df_actual = actual.loc[(actual['talkturn no'] == talkturn_no) &
-                                   (actual['family'] == family), 'text_blob'].values[0]
-            assert (df_actual == df_expected), "not matching!"
-            if df_expected == df_actual:
-                status = 1
-            else:
-                status = 0
+        result = True
 
-            row_i = {'family': family,
-                     'talkturn no': talkturn_no,
-                     'matched': status}
-            matching_status.append(row_i)
+        for index, row in actual[actual['family']=='v']:
+            actual_talkturn_no = row['talkturn no']
+            actual_talkturn = row['text_blob']
+            expected_talkturn = expected.loc[(expected['talkturn_no'] == actual_talkturn_no), 'talkturn'].values[0]
+            if actual_talkturn != expected_talkturn:
+                result = False
 
-    dfr = pd.DataFrame(matching_status)
+        assert result
 
-    return dfr
+    # Test case #2 - vp
+    def test_vp(self):
+        '''
+        Check that verbatim results are all matching
+        :return: assertion
+        '''
 
-if __name__ == '__main__':
-    result = test_blob_accurateness(video_name_1='Ses01F_F', video_name_2='Ses01F_M')
-    print(result)
+        parallel_run_settings = Test_Narrative.parallel_run_settings
+        expected = Test_Narrative.talkturn_lookup
+        actual = main.weave_vpa(video_1='Ses01F_F',
+                                video_2='Ses01F_M',
+                                delay=1,
+                                tone=1,
+                                speech_rate=1,
+                                au_action=0,
+                                posiface=0,
+                                smile=0,
+                                parallel_run_settings=parallel_run_settings
+                                )
+
+        result = True
+
+        for index, row in actual[actual['family']=='vp']:
+            actual_talkturn_no = row['talkturn no']
+            actual_talkturn = row['text_blob']
+            expected_talkturn = expected.loc[(expected['talkturn_no'] == actual_talkturn_no), 'talkturn'].values[0]
+            if actual_talkturn != expected_talkturn:
+                result = False
+
+        assert result
+
+    # Test case #3 - vpa
+    def test_vpa(self):
+        '''
+        Check that verbatim results are all matching
+        :return: assertion
+        '''
+
+        parallel_run_settings = Test_Narrative.parallel_run_settings
+        expected = Test_Narrative.talkturn_lookup
+        actual = main.weave_vpa(video_1='Ses01F_F',
+                                video_2='Ses01F_M',
+                                delay=1,
+                                tone=1,
+                                speech_rate=1,
+                                au_action=1,
+                                posiface=1,
+                                smile=1,
+                                parallel_run_settings=parallel_run_settings
+                                )
+
+        result = True
+
+        for index, row in actual[actual['family']=='vpa']:
+            actual_talkturn_no = row['talkturn no']
+            actual_talkturn = row['text_blob']
+            expected_talkturn = expected.loc[(expected['talkturn_no'] == actual_talkturn_no), 'talkturn'].values[0]
+            if actual_talkturn != expected_talkturn:
+                result = False
+
+        assert result
