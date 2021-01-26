1bf2bba363a3aac88d462944ca7c6d39e82d3b87
Joshua Kim <kim.yee.haun@gmail.com>
2021-01-26 15:29:11 +1100

    z-score pitch more than 3 might be too stringent.

--- Python/Data_Preprocessing/Stage_2/Prosody/talkturn_pitch.py
+++ Python/Data_Preprocessing/Stage_2/Prosody/talkturn_pitch.py
@@ -1,5 +1,11 @@
 '''
 This script computes for pitch per talkturn
+This should be in stage 2
+Because we need:
+(1) the entire speaker wav separated file
+(2) the word level timing
+(3) after the talkturn transcript so that we can get the speaker talkturns and concat them together
+to compute sd and mean
 '''
 import glob
 import os
@@ -11,53 +17,26 @@ import parselmouth
 import Python.Data_Preprocessing.config.dir_config as prs
 
 
-def compute_speaker_pitch_summary(dfr):
+def cartesian_product_basic(left, right):
     '''
-    Compute mean and sd of pitch given the dataframe of wav paths
-    :param dfr:
+    Perfroms basic cross join
+    https://stackoverflow.com/questions/53699012/performant-cartesian-product-cross-join-with-pandas
+    :param left:
+    :param right:
     :return:
     '''
-    speaker_pitch = pd.DataFrame()
-
-    for talkturn_idx in range(len(dfr)):
-        talkturn_i = dfr.iloc[talkturn_idx]
-        snd = parselmouth.Sound(talkturn_i['wav_path'])
-        pitch = snd.to_pitch()
-        pitch_values = pitch.selected_array['frequency']
-        pitch_values[pitch_values == 0] = np.nan
-        pitch_df = pd.DataFrame()
-        pitch_df['time'] = pitch.xs()
-        # the time is based on time in wav (start from zero for every talkturn)
-        # We add the start time of everytalk turn to compute the time in conversation.
-        pitch_df['time'] += talkturn_i['start time']
-        pitch_df['F0'] = pitch_values
-
-        # Drop silences that will affect sd and mean calculation
-        pitch_df = pitch_df[pitch_df['F0'] > 0]
-
-        if len(pitch_df) > 0:
-            speaker_pitch = pd.concat([speaker_pitch, pitch_df], axis=0)
-
-    mean = speaker_pitch['F0'].mean()
-    sd = speaker_pitch['F0'].std()
-
-    return mean, sd
+    return (left.assign(key=1).merge(right.assign(key=1), on='key').drop('key', 1))
 
 
-def compute_coversation_pitch_summary(video_name_1, video_name_2, parallel_run_settings):
+def get_talkturn_and_wav(video_name_1, video_name_2, parallel_run_settings):
     '''
-    We need the speaker-level pitch summary to understand when is considered high pitch
-    for that speaker. Specifically, we need the mean and sd of pitch for that speaker.
+    Get dataframe containing the talkturn transcripts and wav paths
 
-    That means that we should iterate through all extracted talkturn wav fies, so that
-    we consider only audio data from the talkturns f the speaker. We union the pitch data and
-    compute sd and mean from this union pitch data.
     :param video_name_1:
     :param video_name_2:
     :param parallel_run_settings:
     :return:
     '''
-
     # Get the talkturn
     talkturn = pd.read_csv(os.path.join(parallel_run_settings['csv_path'],
                                         video_name_1 + '_' + video_name_2,
@@ -77,65 +56,143 @@ def compute_coversation_pitch_summary(video_name_1, video_name_2, parallel_run_s
     # Inner join between talkturn and wav_files will give us the talkturns that has wav associated
     merged = pd.merge(left=talkturn, right=wav_files, how='inner')
 
+    return merged
+
+
+def compute_speaker_pitch_summary(dfr):
+    '''
+    Compute mean and sd of pitch given the dataframe of wav paths
+    :param dfr:
+    :return:
+    '''
+    speaker_pitch = pd.DataFrame()
+    talkturn_idx = 0
+    for talkturn_idx in range(len(dfr)):
+        talkturn_i = dfr.iloc[talkturn_idx]
+        snd = parselmouth.Sound(talkturn_i['wav_path'])
+        pitch = snd.to_pitch()
+        pitch_values = pitch.selected_array['frequency']
+        pitch_values[pitch_values == 0] = np.nan
+        pitch_df = pd.DataFrame()
+        pitch_df['time'] = pitch.xs()
+        # the time is based on time in wav (start from zero for every talkturn)
+        # We add the start time of everytalk turn to compute the time in conversation.
+        pitch_df['time'] += talkturn_i['start time']
+        pitch_df['F0'] = pitch_values
+        pitch_df['video_id'] = talkturn_i['video_id']
+
+        # Drop silences that will affect sd and mean calculation
+        pitch_df = pitch_df[pitch_df['F0'] > 0]
+
+        if len(pitch_df) > 0:
+            speaker_pitch = pd.concat([speaker_pitch, pitch_df], axis=0)
+
+    mean = speaker_pitch['F0'].mean()
+    sd = speaker_pitch['F0'].std()
+
+    return mean, sd, speaker_pitch
+
+
+def compute_coversation_pitch_summary(video_name_1, video_name_2, parallel_run_settings):
+    '''
+    We need the speaker-level pitch summary to understand when is considered high pitch
+    for that speaker. Specifically, we need the mean and sd of pitch for that speaker.
+    :param video_name_1:
+    :param video_name_2:
+    :param parallel_run_settings:
+    :return:
+    '''
+
+    merged = get_talkturn_and_wav(video_name_1, video_name_2, parallel_run_settings)
+
     # For each speaker, calculate the sd and mean of the pitch
     videos = [video_name_1, video_name_2]
     videos_stats = pd.DataFrame()  # Initiate blank data frame to hold the summary stats
-
+    videos_details = pd.DataFrame()
+    video_idx = 0
     for video_idx in range(len(videos)):
         video = videos[video_idx]
 
         filtered = merged[merged['video_id'] == video]
-        mean, sd = compute_speaker_pitch_summary(dfr=filtered)
+        mean, sd, video_detail = compute_speaker_pitch_summary(dfr=filtered)
 
         # Make a one-row dataframe to be appended
-        video_stat = pd.DataFrame({'video': video,
-                                   'mean': mean,
-                                   'sd': sd}, index=[0])
+        video_stat = pd.DataFrame({'video_id': video,
+                                   'mean_pitch': mean,
+                                   'sd_pitch': sd}, index=[0])
 
         # Append the one-row dataframe
         videos_stats = pd.concat([videos_stats, video_stat])
+        videos_details = pd.concat([videos_details, video_detail])
 
     # Since all rows have index 0, reset index gives it running index.
     videos_stats.reset_index(inplace=True, drop=True)
 
-    return videos_stats
+    return videos_stats, videos_details
 
 
 def annotate_pitch(video_name_1, video_name_2, parallel_run_settings):
-    talkturn = pd.read_csv(os.path.join(parallel_run_settings['csv_path'],
-                                        video_name_1 + '_' + video_name_2,
-                                        "Stage_2",
-                                        'weaved talkturns.csv'))
+    # Get mean and sd by speakers
+    videos_stats, videos_details = compute_coversation_pitch_summary(video_name_1,
+                                                                     video_name_2,
+                                                                     parallel_run_settings)
 
-    videos_stats = compute_coversation_pitch_summary(video_name_1,
-                                                     video_name_2,
-                                                     parallel_run_settings)
+    # Get word level timings with pitch information
+    word_timing = pd.read_csv(os.path.join(parallel_run_settings['csv_path'],
+                                           video_name_1 + '_' + video_name_2,
+                                           "Stage_1",
+                                           'word_transcripts.csv'))
 
-    # PAUSE, I need to visualize the distribution of pitch for all speakers to decide my z-score rule
+    talkturn_wav = get_talkturn_and_wav(video_name_1, video_name_2, parallel_run_settings)
 
+    talkturn_wav.columns = ['video_id', 'audio_id', 'speaker', 'talkturn_no', 'text',
+                            'talkturn_start', 'talkturn_end', 'wav_name', 'wav_path']
 
-def extract_delay(video_name_1, video_name_2, parallel_run_settings):
-    '''
-    Computes for the talkturn delay
-    :return: none
-    '''
-    # parallel_run_settings = prs.get_parallel_run_settings("marriane_win")
-    talkturn = pd.read_csv(os.path.join(parallel_run_settings['csv_path'],
-                                        video_name_1 + '_' + video_name_2,
-                                        "Stage_2",
-                                        'weaved talkturns.csv'))
-    talkturn['previous_speaker'] = talkturn['speaker'].shift()
-    talkturn['next_start_time'] = talkturn['start time'].shift(-1)
-    talkturn['delay'] = talkturn['next_start_time'] - talkturn['end time']
-    talkturn['speaker_match'] = np.where((talkturn['speaker'] != talkturn['previous_speaker']),
-                                         0, 1)
-    idx = np.where((talkturn['speaker_match'] == 0) & (talkturn['delay'] > 0))
-    final_talkturn = talkturn.loc[idx]
-    final_talkturn.to_csv(os.path.join(parallel_run_settings['csv_path'],
-                                       video_name_1 + '_' + video_name_2,
-                                       "Stage_2",
-                                       'talkturn_delay.csv'),
-                          index=False)
+    # Cross join and filter
+    cross_join = cartesian_product_basic(talkturn_wav, word_timing)
+    cross_join = cross_join[cross_join['video_id'] == cross_join['Audio_ID']]
+
+    cross_join['word_in_text'] = cross_join.apply(lambda x: x.word in x.text,
+                                                  axis=1)
+
+    cross_join['wordtime_in_talkturntime'] = cross_join.apply(
+        lambda x: x.start_time >= x.talkturn_start and x.start_time <= x.talkturn_end
+        , axis=1)
+
+    cross_join = cross_join[(cross_join['word_in_text']) * (cross_join['wordtime_in_talkturntime'])]
+
+    # Quality Check - to move to unit test
+    qcdf = cross_join.groupby(['talkturn_no', 'text'])['text'].count()
+    qcdf = qcdf.reset_index(name='nrow')
+    qcdf['text_count'] = qcdf.apply(lambda x: len(x.text.split()), axis=1)
+    assert np.average(qcdf['text_count'] == qcdf['nrow']) == 1.0
+
+    # Get pitch information
+    word_pitches = cartesian_product_basic(cross_join, videos_details)
+    word_pitches.columns
+    word_pitches = word_pitches[word_pitches['video_id_x'] == word_pitches['video_id_y']]
+
+    word_pitches['approximate_time_match'] = word_pitches.apply(
+        lambda x: x.time >= x.start_time - 0.05 and x.time <= x.start_time + 0.05
+        , axis=1)
+
+    word_pitches = word_pitches[word_pitches['approximate_time_match']]
+
+    # Once approximately time matched, compute average across time matches
+    # Intuition: computing the average F0 across +/- 0.05 seconds
+
+    word_stats = word_pitches.groupby(['video_id', 'talkturn_no', 'text', 'word'])[
+        'start_time', 'F0'].mean()
+    word_stats = word_stats.reset_index()
+    word_stats = word_stats.sort_values(['video_id', 'talkturn_no', 'start_time'])
+
+    word_stats = pd.merge(word_stats, videos_stats)
+    word_stats['z_pitch'] = (word_stats['F0'] - word_stats['mean_pitch']) / \
+                            word_stats['sd_pitch']
+
+    # I have visualized the distribution of pitches, mostly unimodal
+    up_arrow = u"\u2191"
+    print(up_arrow + 'hi')
 
 
 if __name__ == '__main__':
