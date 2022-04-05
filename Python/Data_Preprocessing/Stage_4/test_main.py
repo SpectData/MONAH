6cc824a406ba77a584e2cca4666a2519fc6fb8d6
mfmakahiya <44262531+mfmakahiya@users.noreply.github.com>
2022-04-05 03:59:18 +0000

    Tested the code that runs 4 stages continuously

--- Python/Data_Preprocessing/Stage_4/test_main.py
+++ Python/Data_Preprocessing/Stage_4/test_main.py
@@ -1,4 +1,37 @@
 '''
 This script tests the accurateness of the main script results
 '''
+import pandas as pd
+from datetime import datetime
+import Python.Data_Preprocessing.Stage_3.main as m3
+import Python.Data_Preprocessing.Stage_4.main as m4
+import Python.Data_Preprocessing.config.dir_config as prs
+
+if __name__ == '__main__':
+    parallel_run_settings = prs.get_parallel_run_settings("marriane_win")
+    video_list = pd.DataFrame({'video_1': ['Ses01F_impro01_F', 'Ses01F_impro02_F', 'Ses01F_impro03_F', 'Ses01F_impro04_F', 'Ses01F_impro05_F','Ses04F_impro02_F'],
+                               'video_2': ['Ses01F_impro01_M', 'Ses01F_impro02_M', 'Ses01F_impro03_M', 'Ses01F_impro04_M', 'Ses01F_impro05_M','Ses04F_impro02_M']})
+    for i in range(len(video_list)):
+        video_1 = video_list.video_1[i]
+        video_2 = video_list.video_2[i]
+        start = datetime.now()
+        m3.weave_vpa(video_1=video_1, video_2=video_2,
+                  delay=0, tone=1, speech_rate=0,
+                  au_action=1, posiface=1, smile=1, headnod=1, leanforward=1,
+                  parallel_run_settings=parallel_run_settings)
+        m4.weave_session_level_transcript(video_name_1=video_1,
+                                       video_name_2=video_2,
+                                       d_word_count=1,
+                                       p_delay=1,
+                                       p_wpm=1,
+                                       p_tone=1,
+                                       a_au=1,
+                                       a_posiface=1,
+                                       a_smile=1,
+                                       parallel_run_settings=parallel_run_settings
+                                       )
+        print('All Stages Run Time: ', datetime.now() - start)
+        print('Done data processing for ' + video_1 + ' and ' + video_2, '\n')
+
+
 
