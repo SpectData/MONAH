ac9dc06edc0fa58eb0539e2584f174f905321f8b
mfmakahiya <44262531+mfmakahiya@users.noreply.github.com>
2022-04-08 04:58:25 +0000

    Extract audios for selected videos

--- Python/Data_Preprocessing/Stage_1/FFMpeg/execute_extracting_audio.py
+++ Python/Data_Preprocessing/Stage_1/FFMpeg/execute_extracting_audio.py
@@ -28,7 +28,7 @@ def run_extracting_audio(video_name_1, video_name_2, parallel_run_settings):
             video_list.append(file)
 
     video_i = video_list[0]
-    for video_i in video_list:
+    for video_i in [video_name_1, video_name_2]:
         video_path = os.path.join(parallel_run_settings['avi_path'], video_i)
         video_path = os.path.splitext(video_path)[0]
         os.system("ffmpeg -i {0}.avi -f wav -ab 192000 -ac 1 -vn {0}.wav".format(video_path))
